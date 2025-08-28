7515f22d1e9ce34945561d606bc976260c11ce3a
palmar <leo_9319@yahoo.com>
2025-08-28 16:55:24 -0400

    widget in progress

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./HomePage.scss";
-import { Image, Button, Heading, Text } from "@world-vision/wv360-core-library";
+import { Flex, Image, Button, Heading, Text } from "@world-vision/wv360-core-library";
 import * as Tooltip from "@radix-ui/react-tooltip";
 
 import logo from "../../src/assets/images/logo.png";
+import Star from "../../src/assets/images/Star.svg";
 import fallbackImage from "../../src/assets/images/FallbackImage.svg";
 
 const HomePage: React.FC = () => {
@@ -12,6 +13,49 @@ const HomePage: React.FC = () => {
   return (
     <div className="home-page">
       <div className="header-bar">
+        <div className="stat-overlay">
+          <div className="flex">
+            <div>
+              <div className="star-count">
+                <Heading size={{ initial: "6", md: "8" }} weight="bold">
+                  41
+                  <Image
+                    src={Star}
+                    alt="Star"
+                    fallbackSrc={fallbackImage}
+                  />
+                </Heading>
+              </div>
+              <Text align="center">
+                collected
+              </Text>
+            </div>
+            <div>
+              <div className="star-count">
+                <Heading size={{ initial: "6", md: "8" }} weight="bold">
+                  81
+                  <Image
+                    src={Star}
+                    alt="Star"
+                    fallbackSrc={fallbackImage}
+                  />
+                </Heading>
+              </div>
+              <Text align="center">
+                donated
+              </Text>
+            </div>
+          </div>
+          <Text align="center" weight="bold" mt="10px">
+            1 ★ is one tab opened
+          </Text>
+          <Button
+            text="Give Stars"
+            size="md"
+            mode="primary"
+            className="full-width-button"
+          />
+        </div>
         <div className="stat-overlay">
           <Heading size={{ initial: "6", md: "8" }} weight="bold">
             $1280.40
